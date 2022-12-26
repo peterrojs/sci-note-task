74cf11831e1d0e59da7db395205fe2901b61417b
Peter <peter.rojs@gmail.com>
2022-12-26 20:59:32 +0100

    refactor - kotlin data class

--- src/main/kotlin/com/peterrojs/scinotetask/dto/PlateDTO.kt
+++ src/main/kotlin/com/peterrojs/scinotetask/dto/PlateDTO.kt
@@ -1,8 +1,8 @@
 package com.peterrojs.scinotetask.dto
 
-class PlateDTO {
-    var plateSize: Int = 0
-    var sampleArrays: MutableList<List<String>> = mutableListOf()
-    var reagentArrays: MutableList<List<String>> = mutableListOf()
+data class PlateDTO(
+    var plateSize: Int = 0,
+    var sampleArrays: MutableList<List<String>> = mutableListOf(),
+    var reagentArrays: MutableList<List<String>> = mutableListOf(),
     var replicateNumbers: List<Int> = listOf()
-}
+)
